6c36c25540ca9f6b4ca2e8176fe382ecf79c1439
rinfield <rinfield@gmail.com>
2014-12-28 23:38:34 +0900

    Add HTTP Method Override Filter

--- src/main/java/com/github/rinfield/MyApplication.java
+++ src/main/java/com/github/rinfield/MyApplication.java
@@ -5,6 +5,7 @@ import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.hk2.api.DynamicConfigurationService;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.HttpMethodOverrideFilter;
 
 @ApplicationPath("")
 public class MyApplication extends ResourceConfig {
@@ -13,6 +14,9 @@ public class MyApplication extends ResourceConfig {
     public MyApplication(final DynamicConfigurationService dynamicConf)
         throws Exception {
         packages(true, MyApplication.class.getPackage().toString());
+        // Support for HTTP method override via query parameter
+        register(new HttpMethodOverrideFilter(
+            HttpMethodOverrideFilter.Source.QUERY));
         dynamicConf.getPopulator().populate(
             new ClasspathDescriptorFileFinderFix());
     }
---
+++ src/main/java/com/github/rinfield/app/resource/MethodResource.java
@@ -0,0 +1,32 @@
+package com.github.rinfield.app.resource;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+@Path("method")
+public class MethodResource {
+
+    @GET
+    public Response get() {
+        return Response.ok("GET").build();
+    }
+
+    @POST
+    public Response post() {
+        return Response.ok("POST").build();
+    }
+
+    @PUT
+    public Response put() {
+        return Response.ok("PUT").build();
+    }
+
+    @DELETE
+    public Response delete() {
+        return Response.ok("DELETE").build();
+    }
+}
